ecc3b665aedaac739d663989161d4dfacaa0b23a
Okrie <mykkang55@naver.com>
2023-07-23 01:49:27 +0900

    공통적으로 사용할 함수 및 클래스 선언
    - 공용으로 사용할 getJSONData 함수 작성
    - Label color 값 static 선언

---
+++ lib/functions/get_json.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+
+/*
+getJSONData 사용법
+사용할 클래스에서 함수를 하나 선언 ex) getData()
+getData() async {
+  list.addAll(getJSONData('Uri 주소'))
+}
+
+만약 header에 result 나 results 가 있을 경우에는 아래와 같이 사용
+getJSONData('Uri 주소', 'result')
+*/
+
+getJSONData(String uriPath, {String? responseHeader}) async {
+  List data = [];
+  var url = Uri.parse(uriPath);
+  var response = await http.get(url);
+  var convertData = json.decode(utf8.decode(response.bodyBytes));
+  print('Return JSON Type = ${convertData.runtimeType}');
+
+  if(convertData is Map<String, dynamic>){
+    Map<String, dynamic> result = convertData[responseHeader];
+    return result;
+  } else {
+    if(responseHeader == null){
+      List result = convertData;
+      data.addAll(result);
+      return data;
+    } else {
+      List result = convertData[responseHeader];
+      data.addAll(result);
+      return data;
+    }
+  }
+}
---
+++ lib/model/label_color.dart
@@ -0,0 +1,28 @@
+class LabelColors{
+  static Map<String, dynamic> colorLabels = {
+    'pastelRed1' : 0xffffdddd,
+    'pastelRed2' : 0xffffe5dd,
+    'pastelOrange1' : 0xffffeedd,
+    'pastelOrange2' : 0xfffff6dd,
+    'pastelYellow1' : 0xffffffdd,
+    'pastelGreen1' : 0xfff6ffdd,
+    'pastelGreen2' : 0xffeeffdd,
+    'pastelGreen3' : 0xffe5ffdd,
+    'pastelGreen4' : 0xffddffdd,
+    'pastelGreen5' : 0xffddffe5,
+    'pastelGreen6' : 0xffddffee,
+    'pastelGreen7' : 0xffddfff6,
+    'pastelBlue1' : 0xffddffff,
+    'pastelPurple1' : 0xffddf6ff,
+    'pastelPurple2' : 0xffddeeff,
+    'pastelPurple3' : 0xffdde5ff,
+    'pastelPurple4' : 0xffddddff,
+    'pastelPurple5' : 0xffe5ddff,
+    'pastelPurple6' : 0xffeeddff,
+    'pastelPink1' : 0xfff6ddff,
+    'pastelPink2' : 0xffffddff,
+    'pastelPink3' : 0xffffddf6,
+    'pastelPink4' : 0xffffddee,
+    'pastelPink5' : 0xffffdde5,
+  };
+}
