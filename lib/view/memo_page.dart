497fba164a417a004ceeeeaf25d51328c335c869
Okrie <mykkang55@naver.com>
2023-07-26 11:39:57 +0900

    memo widget 분리

--- lib/view/memo_page.dart
+++ lib/view/memo_page.dart
@@ -1,110 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:team_four_todo_list_app/functions/get_json.dart';
-import 'package:team_four_todo_list_app/functions/label_color.dart';
-import 'package:team_four_todo_list_app/view/memo_detail_page.dart';
+import 'package:team_four_todo_list_app/view/widget/memo/memo_main_widget.dart';
 
-class MeMoPage extends StatefulWidget {
+class MeMoPage extends StatelessWidget {
   const MeMoPage({super.key});
-
-  @override
-  State<MeMoPage> createState() => _MeMoPageState();
-}
-
-class _MeMoPageState extends State<MeMoPage> {
-  late List memoData;
-  late int count;
-
-  @override
-  void initState() {
-    super.initState();
-    memoData = [];
-    getData();
-    count = 0;
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          memoData.isEmpty
-          ? const Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircularProgressIndicator(),
-                Text('Loading...')
-              ],
-            ),
-          )
-          : ListView.builder(
-            itemCount: memoData.length,
-            itemBuilder: (context, index) {
-              return Dismissible(
-                direction: DismissDirection.endToStart,
-                key: ValueKey(memoData[index]['userId']),
-                background: const Icon(
-                  Icons.delete_forever,
-                  color: Colors.red,
-                  size: 50,
-                ),
-                child: GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) {
-                          return MemoDetailPage(data: memoData,
-                          memoColor: LabelColors.colorLabels.values.elementAt(
-                            count >= LabelColors.colorLabels.length - 1 ? count = 0 : count++));
-                        },
-                      ),
-                    );
-                  },
-                  child: Card(
-                    color: Color(
-                      LabelColors.colorLabels.values.elementAt(
-                        count >= LabelColors.colorLabels.length -1
-                        ? count = 0
-                        : count++)
-                    ),  //LabelColors.colorLabels['pastelYellow1']),
-                    child: Column(
-                      children: [
-                        Row(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: SizedBox(
-                                width: 350,
-                                child: Text(
-                                  memoData[index]['title'],
-                                  style: const TextStyle(
-                                    fontSize: 25
-                                  ),
-                                )
-                              ),
-                            ),
-                          ],
-                        ),
-                        //Text(memoData[index]['body']),
-                      ],
-                    ),
-                  ),
-                ),
-              );
-            },
-          ),
-        ],
-      ),
+      appBar: AppBar(),
+      body: const MemoMainWidget(),
     );
-  }
-
-
-  getData() async {
-    memoData.addAll(await getJSONData('https://jsonplaceholder.typicode.com/posts'));   // Test 주소
-    print(memoData.length);
-    setState(() {
-      
-    });
   }
 }
